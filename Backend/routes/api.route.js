3021f72ace95a6115dcda9595f640b49ff9083be
Abdullah Patni <abdullahpanja@gmail.com>
2022-10-07 01:06:02 +0530

    Ready

--- Backend/routes/api.route.js
+++ Backend/routes/api.route.js
@@ -4,10 +4,10 @@ const prisma = new PrismaClient();
 
 router.post("/save", async (req, res, next) => {
 	try {
-		const {name, email} = req.body;
+		const { name, email } = req.body;
 		const task = await prisma.user.create({data: { name,email }});
 		res.json(task);
-	} catch (error) {next(error)}
+	} catch (error) {next({message: 'Email Already Taken'})}
 });
 
 module.exports = router;
--- Frontend/src/components/midContent/midContent.jsx
+++ Frontend/src/components/midContent/midContent.jsx
@@ -2,17 +2,30 @@ import { useState } from "react";
 import axios from "axios";
 import road from "../../assets/Pickup_Illustration.png";
 import "./midContent.css";
- import { ToastContainer, toast } from "react-toastify";
- import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MidContent() {
-	const [form, setForm] = useState({name: "",email: ""});
+	const [form, setForm] = useState({ name: "", email: "" });
 
 	const onSubmitForm = (e) => {
 		e.preventDefault();
-		axios.post("http://localhost:4000/save", form).then((response) => {
-			toast("Thank You for Registering");
-		});
+		axios
+			.post("http://localhost:4000/save", form)
+			.then((response) => {
+				toast("Thank You for Registering");
+			})
+			.catch(err => {
+				toast.error(err.response.data.message, {
+					position: "top-right",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+			});
 	};
 
 	const onUpdateField = (e) => {
